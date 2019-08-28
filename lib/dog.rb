d1b6742697c87269adbebfa2436863ac3ac92db2
Steve Alex <steve.alex@hotmail.co.uk>
2019-08-28 10:15:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog
+  def bark
+    "Woof!"
+  end  
+  
+  def sit
+    "The Dog is sitting"
+  end  
+end
---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+  def talk
+    "Hello World!"
+  end
+  
+  def walk
+    "The Person Is Walking"
+  end
+  
+end
